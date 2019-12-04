adcefc9168c8c40d52ab58b79c6731ac110e30b5
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2019-12-04 16:54:59 +0530

    12. Able to get the data inside the typescript

--- typescript-getting-started/app/app.ts
+++ typescript-getting-started/app/app.ts
@@ -1,25 +1,29 @@
-function startGame() {
+function startGame(): void {
     //hello World
-    let name:string = "Nisar";
-    let playerName: string = "Rubicon";
+    let name: string = "Nisar";
+    let playerName: string | undefined = getInputValue("playername");
 
     logPlayer(playerName);
-
-
-    let message: HTMLElement | string ;
-
-    if (typeof message ==='string'){
-        return message
-    }else{
-        return message
-    }
-
-
-    message = document.getElementById("messages");
-    message!.innerText = 'Hello World';
+    postScore(100, playerName)
 }
-function logPlayer(name: string){
+
+function logPlayer(name: string = "Rubicon"): void {
     console.log(`New game has been started by ${name}`)
 }
 
-document.getElementById('startGame')!.addEventListener('click',startGame);
+function getInputValue(elementId: string): string | undefined {
+    const inputElement :HTMLInputElement = document.getElementById(elementId) as HTMLInputElement;
+    if(inputElement.value===""){
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName: string = "Rubicon"):void{
+    const scoreElement:HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+}
+
+document.getElementById('startGame')!.addEventListener('click', startGame);
